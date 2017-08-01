491d35a705258878ceaffb7f69c7be8f6b0a080c
bobaalex <099@tut.by>
2017-08-01 21:35:43 +0300

    Add stars-rating

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 # -- No one
 #
-# insert into app_user(id,name,username,password) values (1,'John Doe','admin','admin');
-# insert into app_user(id,name,username,password) values (2,'Liza Elizabeth','user','user');
+#  insert into app_user(id,name,username,password) values (1,'John Doe','admin','admin');
+#  insert into app_user(id,name,username,password) values (2,'Liza Elizabeth','user','user');
 #
-# insert into app_user_roles(app_user_id,roles) values (1,'ADMIN');
-# insert into app_user_roles(app_user_id,roles) values (1,'USER');
-# insert into app_user_roles(app_user_id,roles) values (2,'USER');
-
+#  insert into app_user_roles(app_user_id,roles) values (1,'ADMIN');
+#  insert into app_user_roles(app_user_id,roles) values (1,'USER');
+#  insert into app_user_roles(app_user_id,roles) values (2,'USER');
+#
 #
 # -- Create Table users for fuck
 # CREATE TABLE users (
