07ff2afd371a4bad7184a623f9bc713dda8a3d97
bobaalex <099@tut.by>
2017-08-05 23:40:52 +0300

    translate

--- src/main/resources/static/app/translate.js
+++ src/main/resources/static/app/translate.js
@@ -13,12 +13,14 @@ angular.module('JWTDemoApp')
             HI: 'Hi',
             LOGOUT: 'Logout',
             LOGIN: 'Login',
+            SUBMIT: 'Submit',
             //users
             USER: 'User',
             NAME: 'Name',
             USERNAME: 'Username',
             ROLE: 'Role',
             PASSWORD: 'Password',
+            CONFIRM_PASSWORD: 'Confirm password',
             BUTTON_CREATE_USER: 'Create',
             BUTTON_UPDATE_USER: 'Update',
             USERS: 'Users',
@@ -35,7 +37,15 @@ angular.module('JWTDemoApp')
             NEW_INSTRUCTION: 'New instruction',
             NUMBER_OF_STEPS: 'Number of steps',
             NUMBER_OF_COMMENTS: 'Number of comments',
-            DATE_OF_CREATION: 'Date of creation'
+            DATE_OF_CREATION: 'Date of creation',
+            //profile
+            ABOUT_ME: 'About me',
+            ADDRESS: 'Address',
+            BRONZE_INSTRUCTION_CREATOR: 'Bronze instruction creator',
+            FOR_MAKE_0_INSTRUCTIONS: 'For make 0 instructions!',
+            BRONZE_STEP_CREATOR: 'For make 0 steps!',
+            ACHIEVEMENTS: 'Achievements'
+
 
         });
         // Русский
@@ -48,12 +58,14 @@ angular.module('JWTDemoApp')
             HI: 'Привет',
             LOGOUT: 'Выйти',
             LOGIN: 'Войти',
+            SUBMIT: 'Подтвердить',
             //users
             USER: 'Пользователь',
             NAME: 'Имя',
             USERNAME: 'Логин',
             ROLE: 'Роль',
             PASSWORD: 'Пароль',
+            CONFIRM_PASSWORD: 'Подтвердите пароль',
             BUTTON_CREATE_USER: 'Создать',
             BUTTON_UPDATE_USER: 'Обновить',
             USERS: 'Пользователи',
@@ -70,7 +82,14 @@ angular.module('JWTDemoApp')
             NEW_INSTRUCTION: 'Новая инструкция',
             NUMBER_OF_STEPS: 'Количество шагов',
             NUMBER_OF_COMMENTS: 'Количество комментариев',
-            DATE_OF_CREATION: 'Дата создания'
+            DATE_OF_CREATION: 'Дата создания',
+            //profile
+            ABOUT_ME: 'Обо мне',
+            ADDRESS: 'Адрес',
+            BRONZE_INSTRUCTION_CREATOR: 'Бронзовый создатель инструкций',
+            FOR_MAKE_0_INSTRUCTIONS: 'За 0 инструкций!',
+            BRONZE_STEP_CREATOR: 'За 0 шагов!',
+            ACHIEVEMENTS: 'Награды'
         });
         // Устанавливаем язык по умолчанию
         $translateProvider.preferredLanguage('en');
